82c787a07ceda4442771a630373ad700ba1a80af
Chrisdocs <chrisa.wolfe@gmail.com>
2022-02-18 10:55:34 -0600

    created hiit timer page, removed uneeded links on home page, text updates

---
+++ components/layout.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+export const siteTitle = 'TimeHiit'
+
+export default function Layout({ children, home }) {
+    return (
+        <div>
+            <Head>
+
+            </Head>
+        </div>
+    )
+}
---
+++ pages/timers/hiit-timer.js
@@ -0,0 +1,15 @@
+// import Head from 'next/head';
+// import Image from 'next/image';
+// import styles from './layout.module.css';
+// import utilStyles from '../styles/utils.module.css';
+// import Layout, { siteTitle } from '../components/layout';
+// import Link from 'next/link';
+
+export default function HiitTimer() {
+
+    return (
+        <div>
+            <p>timer</p>
+        </div>
+    )
+}
